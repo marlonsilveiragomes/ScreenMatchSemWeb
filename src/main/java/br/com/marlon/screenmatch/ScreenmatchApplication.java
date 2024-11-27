db8ec3194a683f2b877c4ececa1728fb8fa2cec7
Marlon Silveira Gomes <marlonsilveiragomes@gmail.com>
2024-11-27 16:01:35 -0300

    Inclusão de lambdas, streams e Spring Boot framework

--- src/main/java/br/com/marlon/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/marlon/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,9 @@
 package br.com.marlon.screenmatch;
 
-import br.com.marlon.screenmatch.model.DadosEpisodio;
-import br.com.marlon.screenmatch.model.DadosSerie;
-import br.com.marlon.screenmatch.model.DadosTemporada;
 import br.com.marlon.screenmatch.principal.Principal;
-import br.com.marlon.screenmatch.service.ConsumoApi;
-import br.com.marlon.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
